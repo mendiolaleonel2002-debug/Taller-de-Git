859c55e9a8abec9b96c2be82f0bc41b0f38f4497
Tetsuo <azif___@hotmail.com>
2026-02-27 18:45:17 -0600

    Edición de hello.py para el taller

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hola mi nombre es Leonel y esta es mi colaboración :P")
+print ("Recuerden seguirse en sus perfiles de GitHub, hacer colaboración y obtener su certificado!!!")
+print('Estás es mi aportación Heathcliff')
